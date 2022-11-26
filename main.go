2e1faf16d22f8975ba4e07345236deb07b4119ff
henrique-alexandre <henriquealexandre@gmail.com>
2022-11-26 14:18:12 +0000

    converting handleFunc to be used as a basic router

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Get in touch</h1>")
 }
 
-type Router struct{}
+/*type Router struct{}
 
 func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
@@ -26,10 +26,24 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 		return
 	}
+}*/
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.NotFound(w, r)
+		return
+	}
+
 }
 
 func main() {
-	router := Router{}
+	//router := Router{}
 	fmt.Println("Starting server on port 3333")
-	http.ListenAndServe(":3333", router)
+	//http.ListenAndServe(":3333", router)
+	http.ListenAndServe(":3333", http.HandlerFunc(pathHandler)) // we can convert a handleFunc to be used as a handler
 }
