a1ece4412a8a99635dc66c6f0c28c44ef44e5e9e
henrique-alexandre <henriquealexandre@gmail.com>
2022-11-26 18:10:38 +0000

    Implementing very basic chi router

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,36 +16,10 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Get in touch</h1>")
 }
 
-/*type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		return
-	}
-}*/
-
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.NotFound(w, r)
-		return
-	}
-
-}
-
 func main() {
-	//router := Router{}
-	fmt.Println("Starting server on port 3333")
-	//http.ListenAndServe(":3333", router)
-	http.ListenAndServe(":3333", http.HandlerFunc(pathHandler)) // we can convert a handleFunc to be used as a handler
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.NotFound(http.NotFound)
+	http.ListenAndServe(":3333", r)
 }
