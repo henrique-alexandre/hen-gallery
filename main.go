5535e862e1a3b885dcde5d8510bd799bb6d188d1
henrique-alexandre <henriquealexandre@gmail.com>
2022-11-26 18:40:17 +0000

    Implementing Chi Logger Middleware

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +19,7 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.NotFound(http.NotFound)
